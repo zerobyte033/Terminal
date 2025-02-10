89945f1731b3841e1e81cfdca7eee9359b47ba0f
zerobyte033 <mdvai1515@gmail.com>
2025-02-11 02:52:55 +0600

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,242 @@
+import os,sys,time,json,random,re,string,platform,base64,uuid,getpass
+#-----------color-----------------#
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+
+# Bold
+bblack="\033[1;30m"       # Black
+bred="\033[1;31m"         # Red
+bgreen="\033[1;32m"       # Green
+byellow="\033[1;33m"      # Yellow
+bblue="\033[1;34m"        # Blue
+bpurple="\033[1;35m"      # Purple
+bcyan="\033[1;36m"        # Cyan
+bwhite="\033[1;37m"       # White
+
+#-------clear def-------#
+def clear():
+	os.system('pkg install espeak')
+	os.system("clear")
+from os import system as _SUYAIB_
+print("\033[1;92m{}>..𝐂𝐇𝐄𝐀𝐊𝐈𝐍𝐆 𝐔𝐏𝐃𝐀𝐓𝐄...")
+os.system('espeak -a 300 "cheaking.  update.    "')
+#os.system("pkg install espeak")
+print("\033[1;92m{}>..𝐈𝐀𝐌 𝐓𝐄𝐑𝐌𝐔𝐗 𝐅𝐔𝐋𝐋 𝐒𝐄𝐓𝐔𝐏 𝐓𝐎𝐎𝐋𝐒...")
+os.system('espeak -a 300 " i am.    termux   full   setup   tools    "')
+print("\033[1;92m{}>..𝐈 𝐌𝐀𝐃𝐄 𝐁𝐘 𝐌𝐃 𝐒𝐔𝐘𝐀𝐈𝐁 𝐈𝐒𝐋𝐀𝐌...")
+os.system('espeak -a 300 " i.     made.    by.    Zero. Byte.    "')
+
+#os.system("git pull")
+#os.system("pip install bs4")
+#os.system("pip install requests")
+#os.system("pip install mechanize")
+
+print("\033[1;92m{}>..𝐂𝐘𝐁𝐄𝐑 𝐊𝐈𝐍𝐆 𝐒𝐔𝐘𝐀𝐈𝐁 (𝐑𝐎𝐁𝐎𝐓) 𝐒𝐘𝐒𝐓𝐄𝐌 𝐈𝐍𝐒𝐓𝐀𝐋𝐈𝐍𝐆....\033[1;30m")
+os.system('espeak -a 300 "Cyber.   King.   shuaib.   Robot.   system.   installing "')
+os.system("git pull")
+print("\033[1;92m{}>..𝐑𝐎𝐁𝐎𝐓 𝐈𝐍𝐒𝐓𝐀𝐋𝐋 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐄..{√}\033[1;30m")
+os.system('espeak -a 300 "Robot.   install.   complete"')
+print("\033[1;92m{}>..𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝐀𝐈𝐓 𝐅𝐎𝐑 𝐋𝐎𝐀𝐃𝐈𝐍𝐆.....\033[1;30m")
+os.system('espeak -a 300 "Please.   WAIT.   FOR.   LOADING...."')
+#os.system("pip install bs4")
+import os,requests,time
+time.sleep(1)
+os.system("clear")
+
+def main():
+	os.system('clear')
+os.system('clear')
+
+logo = ("""
+\033[1;35m
+ _       __________    __    __________  __  _________
+| |     / / ____/ /   / /   / ____/ __ \/  |/  / ____/
+| | /| / / __/ / /   / /   / /   / / / / /|_/ / __/   
+| |/ |/ / /___/ /___/ /___/ /___/ /_/ / /  / / /___   
+|__/|__/_____/_____/_____/\____/\____/_/  /_/_____/   
+                                                     
+         
+
+      \033[0;37m
+  __________ ____   ___  ______   _______ _____ 
+ |__  / ____|  _ \ / _ \| __ ) \ / /_   _| ____|
+   / /|  _| | |_) | | | |  _ \\ V /  | | |  _|  
+  / /_| |___|  _ <| |_| | |_) || |   | | | |___ 
+ /____|_____|_| \_\\___/|____/ |_|   |_| |_____|
+                                                
+ """)
+menu = ("""
+
+[𝟬𝟭]---𝐓𝐄𝐑𝐌𝐔𝐗 𝐁𝐀𝐒𝐈𝐂 𝐒𝐄𝐓𝐔𝐏
+[𝟬𝟮]---𝐓𝐄𝐑𝐌𝐔𝐗 𝐅𝐔𝐋𝐋 𝐒𝐄𝐓𝐔𝐏
+[𝟬𝟯]---𝐎𝐔𝐑 𝐓𝐄𝐋𝐄𝐆𝐄𝐀𝐌 𝐂𝐇𝐀𝐍𝐍𝐄𝐋
+[𝟬𝟰]---𝐌𝐘 𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 𝐈𝐃
+[𝟬𝟱]---𝐌𝐘 𝐖𝐇𝐀𝐓𝐒 𝐀𝐏𝐏 𝐈𝐃
+[𝟎𝟎]---𝐓𝐄𝐑𝐌𝐔𝐗 𝐏𝐄𝐑𝐌𝐈𝐒𝐒𝐈𝐎𝐍
+
+""")
+
+def lock():
+	option = input("""
+{⚜}⚜️ ENTER PASSWORD: """)
+	if option in ['Fahad','fahad','FAHAD']:
+		os.system('clear')
+		moon()
+	else:
+		print("😁😁𝐒𝐎𝐑𝐑𝐘...𝐘𝐎𝐔 𝐀𝐑𝐄 𝐖𝐑𝐎𝐍𝐆 𝐏𝐄𝐑𝐒𝐎𝐍....")
+def moon():
+	print(logo)
+	print(menu)
+	xd = input("[✔️].....𝐒𝐄𝐋𝐄𝐂𝐓 𝐎𝐏𝐓𝐈𝐎𝐍 :")
+	if xd in ['1','01','A','a']:
+		print("𝐓𝐄𝐑𝐌𝐔𝐗 𝐁𝐀𝐒𝐈𝐂 𝐒𝐄𝐓𝐔𝐏 𝐒𝐓𝐀𝐑𝐓𝐈𝐍𝐆.....")
+		basic()
+		print('𝐓𝐄𝐑𝐌𝐔𝐗 𝐁𝐀𝐒𝐈𝐂 𝐒𝐄𝐓𝐔𝐏 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐄')
+	if xd in ['2','02','B','b']:
+		print("𝐓𝐄𝐑𝐌𝐔𝐗 𝐅𝐔𝐋𝐋 𝐒𝐄𝐓𝐔𝐏 𝐒𝐓𝐀𝐑𝐓𝐈𝐍𝐆......")
+		full()
+		print('𝐓𝐄𝐑𝐌𝐔𝐗 𝐅𝐔𝐋𝐋 𝐒𝐄𝐓𝐔𝐏 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐄')
+	if xd in ['3','03','C','c']:
+		print("🫡𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝐀𝐈𝐓.......")
+		channel()
+		print("🫡𝐋𝐎𝐀𝐃𝐈𝐍𝐆 𝐒𝐔𝐂𝐂𝐄𝐒𝐒𝐅𝐔𝐋𝐋....")
+	if xd in ['4','04','D','d']:
+		print("🫡𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝐀𝐈𝐓...")
+		telegram()
+		print("🫡𝐋𝐎𝐀𝐃𝐈𝐍𝐆 𝐒𝐔𝐂𝐂𝐄𝐒𝐒𝐅𝐔𝐋𝐋....")
+	if xd in ['5','05','E','e']:
+		print("🫡𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝐀𝐈𝐓.....")
+		whats()
+		print("😭😭𝐋𝐎𝐀𝐃𝐈𝐍𝐆 𝐔𝐍𝐒𝐔𝐂𝐂𝐄𝐒𝐒....")
+	if xd in ['0','00']:
+		permission()
+
+def basic():
+	os.system('termux-setup-storage')
+	os.system("""
+	pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install python3
+pkg install wget
+pkg install git
+pkg install bash
+pkg install python-pip
+pip2 install wget
+pip install bs4
+pip2 install bs4
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install php
+pip2 install php""")
+def full():
+	os.system('termux-setup-storage')
+	os.system("""
+	pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install python3
+pkg install python-pip
+pkg install wget
+pkg install fish
+pkg install ruby
+pkg install help
+pkg install git
+pkg install dnsutils  
+pkg install php
+pkg install perl
+pkg install lua
+pkg install parallel
+pkg install nmap
+pkg install bash
+pkg install clang
+pkg install nano
+pkg install w3m
+pkg install hydra
+pkg install figlet
+pkg install cowsay
+pkg install curl
+pkg install tar
+pkg install zip
+pkg install unzip
+pkg install net-tools
+pkg install tor -y
+pkg install sudo -y
+pkg install wireshark
+pkg install wgetrc
+pkg install wcalc
+pkg install openssl
+pkg install openssl-tool
+pkg install bmon
+pkg install vpn
+pkg install unrar
+pkg install toilet
+pkg install proot
+pkg install net-tools
+pkg install vim
+pkg install vim-python
+pkg install ired
+pkg install goaccess
+pkg install golang
+pkg install tar
+pkg install kibi
+pkg install tsu
+pkg install tmux
+pkg install mtools
+pkg install file
+pkg install vis
+pkg install pass
+pkg install pick
+pkg install chroot
+termux-chroot
+pkg install macchanger
+pkg install ninja
+pkg install elixir
+pkg install swift
+pkg install xmlstarlet
+pkg install fakeroot
+pkg install texinfo
+pkg install netcat
+pkg install wren
+pkg install gatling
+pkg install cvs
+pkg install ffmpeg
+pkg install screen 
+pkg install neofetch 
+pkg install mariadb
+pkg install picolisp
+pkg install toilet
+pkg install cmatrix
+pkg install dropbear
+pkg install openssh
+pkg install python-pip
+pip2 install wget
+pip install bs4
+pip2 install bs4
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install php
+pip2 install php""")
+
+def channel():
+	os.system('xdg-open https://t.me/Ethereum_Hacker_Main')
+def telegram():
+	os.system('xdg-open https://t.me/@Unknown_x05')
+def whats():
+	print("𝐒𝐎𝐑𝐑𝐘 𝐅𝐎𝐑 𝐓𝐇𝐀𝐓")
+def permission():
+	os.system('termux-setup-storage')
+
+lock()
